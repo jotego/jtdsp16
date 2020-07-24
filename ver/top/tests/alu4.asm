5083f4ebef3ac1534258752c0d7fbe7b25b9611b
Jose Tejada <jotego@gmail.com>
2020-07-24 15:26:15 +0200

    aD=aS+y (F1=1101)

---
+++ ver/top/tests/alu4.asm
@@ -0,0 +1,20 @@
+y=0xa
+r0=0
+*r0++=y
+y=0xb
+*r0++=y
+y=0xc
+*r0++=y
+y=0xd
+*r0=y
+y=0
+# Sum everything now
+r0=0
+y=*r0++ # a
+a0=a0+y
+y=*r0++ # b
+a0=a0+y
+y=*r0++ # c
+a0=a0+y
+y=*r0++ # d
+a0=a0+y
